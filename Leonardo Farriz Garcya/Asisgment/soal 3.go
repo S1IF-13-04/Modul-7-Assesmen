fb81724d73e42260fa512a3f6ee010abd1b54d05
leonardo0138 <leonardofarriz19@gmail.com>
2025-10-27 09:08:48 +0700

    Add files via upload

---
+++ Leonardo Farriz Garcya/Asisgment/soal 3.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main (){
+	var peti,karung,ikat, keping int
+	fmt.Print("masukan berapa ikat: ")
+	fmt.Scan(&keping)
+
+	peti = keping/800
+	karung = (keping%800)/100
+	ikat = (keping%100)/10
+	keping = keping%10
+
+	fmt.Println(peti,"peti",karung,"karung",ikat,"ikat",keping,"keping")
+}
