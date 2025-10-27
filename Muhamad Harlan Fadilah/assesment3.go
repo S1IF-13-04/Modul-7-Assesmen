7ffe6561879208aae1e5bab8d299a18c51af8bc9
Muhamad Harlan Fadilah <unityroblox@gmail.com>
2025-10-27 08:57:04 +0700

    Add files via upload

---
+++ Muhamad Harlan Fadilah/assesment3.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var keping int
+	fmt.Print("Masukkan jumlah keping: ")
+	fmt.Scan(&keping)
+
+	peti := keping / 800
+	sisa := keping % 800
+
+	karung := sisa / 80
+	sisa = sisa % 80
+
+	ikat := sisa / 8
+	sisa = sisa % 8
+
+	fmt.Printf("%d peti, %d karung, %d ikat, dan %d keping\n", peti, karung, ikat, sisa)
+}
